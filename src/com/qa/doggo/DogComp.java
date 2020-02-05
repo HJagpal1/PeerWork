98c703e217ce9a798c4eb1bd82d35a886f0ba2c5
Harjeevan <HarjeevanJagpal01@gmail.com>
2020-02-05 15:34:35 +0000

    doggo

--- src/com/qa/doggo/DogComp.java
+++ src/com/qa/doggo/DogComp.java
@@ -1,20 +1,33 @@
 package com.qa.doggo;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DogComp {
 	
 	
 	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);//pos = position
-		int x = in.nextInt();
-		for (int i = 0;  i < 100; i++) {
+		int number;
+		Scanner in = new Scanner(System.in);
+//		Scanner in = new Scanner(System.in);//pos = position
+//		int x = in.nextInt();
+//		for (int i = 0;  i < 100; i++) {
 			
+			ArrayList<Integer>doggo = new ArrayList<Integer>();
 			
-			
+			for (int i = 0;  i <= 100; i++) {
+			doggo.add(i);
 		}
+			
+//			for (i : doggo) {
+//				if (i= number)
+//					break;
+//				else{ System.out.println());
+//			}}
+			
+			System.out.println("Dog is in position : " + in);
 		
+			System.out.println(doggo);
 	}
-
 }
+
